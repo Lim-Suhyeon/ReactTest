872e1b3910372c8dd3ad0c9789b925bcaae9106e
Lim-Suhyeon <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-17 16:07:48 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { createGlobalStyle } from "styled-components";
+import TodoHead from "./components/TodoHead";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+export default function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <TodoHead />
+    </div>
+  );
+}

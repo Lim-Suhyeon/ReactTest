15ab477910018665bd7e57f2a053ba7f007d372f
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-17 07:13:13 +0000

    Update 2 files

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -28,7 +28,7 @@ const TodoHeadBlock = styled.div`
 function TodoHead() {
   return (
     <TodoHeadBlock>
-      <h1>2019년 7월 10일</h1>
+      <h1>2019년 7월 10일 git 테스트</h1트
       <div className="day">수요일</div>
       <div className="tasks-left">할 일 2개 남음</div>
     </TodoHeadBlock>

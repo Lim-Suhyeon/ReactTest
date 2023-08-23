db81dbe97b60eb6a2cdcfe98a8c2a486ff802c25
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-23 05:46:34 +0000

    Update 6 files

--- src/component/Input.js
+++ src/component/Input.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Input(props) {
   //btn_del show
-  const onChange = (event) => {
-    console.log(event.target);
+  const [isDelbtn, setActive] = useState(false);
+
+  const onChange = (e) => {
+    const $value = e.target.value.length;
+    if ($value > 0) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
   };
   return (
     <div className="inp_wrap">
       <div className="inp_box">
         <input
+          value={props.value}
           placeholder={props.placeholder}
           type={props.type}
           onChange={onChange}
         />
-        <button className="btn_del" />
+        <button className={`btn_del ${isDelbtn ? "active" : ""}`}>삭제</button>
       </div>
     </div>
   );

6f9c93867b0f149edf22108f43e16741b661ed7a
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-23 01:27:03 +0000

    Update Input.js and InputGuide.js

--- src/component/Input.js
+++ src/component/Input.js
@@ -3,11 +3,17 @@ import React from "react";
 function Input(props) {
   return (
     <div className="input_wrap">
-        <div className="">
-            <input value="test" type="text" />
-        </div>
+      <div className="">
+        <input placeholder={props.placeholder} type={props.type} />
+        <button className="btn_del" />
+      </div>
     </div>
   );
+}
+
+Input.defaultProps = {
+    type: 'text',
+    placeholder: ''
 }
 
 export default Input;

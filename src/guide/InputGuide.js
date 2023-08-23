e320428f33eebaba9d902e4b0a9a63e8d6d3adb8
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-23 00:56:20 +0000

    Update 3 files

--- src/guide/InputGuide.js
+++ src/guide/InputGuide.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Input from "../component/Input";
 
 function InputGuide(props) {
   return (
     <div className="guide_cont">
       <h2 className="guide_h2">Input</h2>
+      <Input />
     </div>
   );
 }

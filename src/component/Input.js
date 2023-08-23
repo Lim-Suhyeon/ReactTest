9dca7df40e29fa51bfe84b2f766e6ef753d54d6c
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-23 08:08:51 +0000

    Update 3 files

--- src/component/Input.js
+++ src/component/Input.js
@@ -3,15 +3,6 @@ import React, { useState, useEffect } from "react";
 function Input(props) {
   //btn_del show
   const [isDelbtn, setActive] = useState(false);
-  useEffect(() => {
-    console.log("컴포넌트가 화면에 나타남");
-    return () => {
-      console.log("컴포넌트가 화면에서 사라짐");
-    };
-  }, []);
-  const init = () => {
-    alert();
-  };
   const onChange = (e) => {
     const $value = e.target.value.length;
     if ($value > 0) {
@@ -25,12 +16,17 @@ function Input(props) {
     <div className="inp_wrap">
       <div className="inp_box">
         <input
-          value={props.value}
+          defaultValue={props.defaultValue}
           placeholder={props.placeholder}
           type={props.type}
           onChange={onChange}
         />
-        <button className={`btn_del ${isDelbtn ? "active" : ""}`}>삭제</button>
+        <button
+          className={`btn_del ${isDelbtn ? "active" : ""}
+         ${props.isDelbtn ? "active" : ""}`}
+        >
+          삭제
+        </button>
       </div>
     </div>
   );
@@ -39,6 +35,7 @@ function Input(props) {
 Input.defaultProps = {
   type: "text",
   placeholder: "",
+  isDelbtn: false,
 };
 
 export default Input;

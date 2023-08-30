f0cc1e7ffe4bd17c66ad2793099671094390a64f
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-30 01:58:56 +0000

    Update 3 files

---
+++ src/component/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button(props) {
+  return (
+    <div className="guide_cont">
+      <div className={"btn_wrap " + props.className}>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Button;
--- src/guide/ButtonGuide.js
+++ src/guide/ButtonGuide.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Button from "../component/Button";
 
 function ButtonGuide(props) {
   return (
     <div className="guide_cont">
       <h2 className="guide_h2">Button</h2>
+      <Button className="col02" />
     </div>
   );
 }

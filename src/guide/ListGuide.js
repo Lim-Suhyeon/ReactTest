c579106d26d34c2dff2051affed3b20765c87b31
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-31 07:07:55 +0000

    Update 3 files

--- src/guide/ListGuide.js
+++ src/guide/ListGuide.js
@@ -2,12 +2,17 @@ import React from "react";
 import List from "../component/List";
 
 function ListGuide() {
+  const dataList = [
+    { name: "김철수", age: "20" },
+    { name: "김영희", age: "21" },
+    { name: "매머드", age: "22" },
+  ];
+
   return (
     <div className="guide_cont">
       <h2 className="guide_h2">List</h2>
-      <List />
+      <List dataList={dataList} />
     </div>
   );
 }
-
 export default ListGuide;

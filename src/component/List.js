e67ba445b8393af4bde300f6d9e4bb6872ad9949
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-08-31 01:58:03 +0000

    Update 2 files

--- src/component/List.js
+++ src/component/List.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ListItem from "./ListItem";
 
 function List(props) {
-  return <div className="guide_cont"></div>;
+  const MENU_LIST = [
+    { title: "TAMS", list: ["Users", "Wallets"] },
+    { title: "Jupiter", list: ["Create", "Read", "Update", "Delete"] },
+  ];
+  return (
+    <div className="guide_cont">
+      <ul>
+        {MENU_LIST.map((item, idx) => {
+          return (
+            // 1단 메뉴 부분
+            // ListItem 하나 하나가 <li>이다
+            <ListItem title={item.title} idx={idx} list={item.list} />
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default List;

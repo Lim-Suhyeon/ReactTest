9ef1d9197169014133862224db289f85ef94a342
임수현 <142466249+Lim-Suhyeon@users.noreply.github.com>
2023-09-01 06:04:33 +0000

    Update 3 files

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -2,13 +2,23 @@ import React from "react";
 
 function ListItem(props) {
   const lableBox = props.label;
-  const labelList = lableBox.map((data) => (
-    <span>{data}</span>
-  ));
+  const labelList = lableBox.map((label,idx) => {
+    let $class;
+    const $label = label;
+    if($label == '고위험') {
+      $class = 'red';
+    }else {
+      $class = 'green';
+    }
+    return (<div key={idx} className={$class}>{label}</div>)
+  });
+
   return (
     <>
-      <li>
-        <div>{labelList}</div>
+      <li ket={props.idx}>
+        <div className="wrap_label">
+          {labelList}
+        </div>
         {props.name}, {props.date}
       </li>
     </>
